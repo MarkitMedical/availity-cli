89bc63605ff0a7c763bd8b02b817878d89de3aed
Rob Warner <rob.warner@availity.com>
2015-03-11 14:36:51 -0400

    Close #23. Delete health check test

--- test/health.check.test.js
+++
@@ -1,9 +0,0 @@
-var healthCheck = require('../lib/health.check');
-
-describe('health check', function() {
-  this.timeout(10000); // Allow the CLIs to run to completion
-
-  it('should be healthy', function() {
-    healthCheck.isHealthy().should.be.ok;
-  });
-});
